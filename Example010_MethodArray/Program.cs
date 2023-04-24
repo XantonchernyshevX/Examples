eb8deb4bd52d1a9eedd8f84beb5ba472902b9222
Anton_Chernyshev <playhardcoreordie@gmail.com>
2023-04-24 10:11:53 +0300

    изменение

--- Example010_MethodArray/Program.cs
+++ Example010_MethodArray/Program.cs
@@ -1,11 +1,11 @@
-﻿int[] array = {1,12,31,4,18,16,17,18};
+﻿int[] array = { 1, 12, 31, 4, 18, 16, 17, 18 };
 int n = array.Length;
 int find = 18;
 int index = 0;
 
 while (index < n)
 {
-    if(array[index] == find)
+    if (array[index] == find)
     {
         Console.WriteLine(index);
         break;
